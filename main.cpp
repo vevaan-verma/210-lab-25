82273666a85484a30b048698041ac04ea461738b
Vevaan <vevaan.s.verma@gmail.com>
2024-10-25 22:50:40 -0700

    Begin vector race

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
-#include <iostream>
+// COMSC-210 | Lab 25 | Vevaan Verma
 #include <chrono>
+#include <iostream>
+#include <vector>
+#include <fstream>
 using namespace std;
+using namespace std::chrono;
 
+/* FUNCTION PROTOTYPES */
+long long raceVector();
+
+// main() is the entry point of the program and drives the program
+// arguments: none
+// returns: int - the exit code of the program
 int main() {
-    
 
-    return 0;
+	cout << "Time taken by vector: " << raceVector() << " milliseconds" << endl; // output duration
+
+	return 0;
+
+}
+
+long long raceVector() {
+
+	auto start = high_resolution_clock::now(); // start time
+
+	vector<int> v;
+
+	auto end = high_resolution_clock::now(); // end time
+	auto duration = duration_cast<milliseconds>(end - start); // duration
+	return duration.count(); // return duration
+
 }
 
 /* syntax examples:
